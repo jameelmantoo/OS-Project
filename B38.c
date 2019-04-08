1d04204a64c6bbda5ab0a41dff1aac2d9f29ce2d
jameelmantoo <jameelmantoo7@gmail.com>
2019-04-09 00:33:04 +0530

    Update B38.c

--- B38.c
+++ B38.c
@@ -4,24 +4,25 @@
 #include <math.h>
 #include <stdlib.h>
 
-int i         = 0;	
-int amtWanted = 0;
-int totalPts  = 0;
+int i = 0;	
+int amt = 0;
+int total = 0;
 
 void *count(void *X)
-{ 
-    for (i=0; i < amtWanted; i++)
-    {	
+{
+	for (i=0; i < amt; i++)
+	{
         double X = (double)rand() / RAND_MAX;
         double Y = (double)rand() / RAND_MAX;
         
         if (((X * X) + (Y * Y)) <= 1)
-        {
-            totalPts++;
-        }
-    }
-    return NULL;
+        	{
+	 		total++;
+        	}
+    	}
+	return NULL;
 }
+
 
 int main()
 {
@@ -29,18 +30,18 @@ int main()
     srand(time(NULL));
     pthread_t thread;
 
-    do
+	do
 	{
         printf("Please enter a positive number of points you would like to generate? \n");
-        scanf("%d", &amtWanted);  	
+        scanf("%d", &amt);  	
 	}
-    while (amtWanted <= 0);
-  
+	while (amt <= 0);
+
     pthread_create(&thread, NULL, &count, NULL);
     pthread_join(thread, NULL);
     
-    double points = 4.0 * totalPts;
-    double pi = points / amtWanted;
-    printf("The approximate value of Pi for the desired amount of points (%d) is: %f \n", amtWanted, pi);  
+    double points = 4.0 * total;
+    double pi = points / amt;
+    printf("The approximate value of Pi for the desired amount of points (%d) is: %f \n", amt, pi);  
     return 0;
 }
